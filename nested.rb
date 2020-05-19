82e76cf9cf795b29dd8fb1b26f0f9e3dcbd39896
eager-class-5814 <candacehchoe@gmail.com>
2020-05-19 20:11:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,12 +15,12 @@ def hopper
         :languages => ["C"]
       }
     }
-
+puts programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	puts programmer_hash[:alan_kay]
+	puts programmer_hash[:alan_kay][:known_for]
 	programmer_hash = 
  		{
       :grace_hopper => {
